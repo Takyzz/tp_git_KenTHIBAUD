1dad2433f0382e43f1e23ba0a909e493848697e9
takyz <addiken01@gmail.com>
2022-07-20 17:38:47 +0400

    Implémentation code pour insérer valeur

--- addTwoInt.py
+++ addTwoInt.py
@@ -12,5 +12,10 @@ if l == 3 :
 
     print("Somme = ",add(a,b))
 
+elif l == 1 or l ==0:
+    a = int(input("Entrez premier nombre:"))
+    b = int(input("Entrez deuxième nombre: "))
+    print("Somme = ",add(a,b))
+
 else:
     print("Erreur !! Il faut insérer 2 valeurs.")
